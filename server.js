d69e4d02b19fa6c7ce04e179f0c7c35c5fc24449
Christopher Glenn <Chrisglenn519@yahoo.com>
2020-02-12 17:46:26 -0600

    connected to db

--- server.js
+++ server.js
@@ -3,15 +3,22 @@ const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const port = process.env.PORT || 3001
-const Account = ('./routes/account')
+const db = require('./models')
 
 app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
 
-// app.use('/account',Account)
 require('./routes/account')(app)
 
-app.listen(port, () =>{
-    console.log('app is listening on port: '+ port)
-})
+let syncOptions = {force: false}
+
+if(process.env.NODE_ENV === 'test'){
+    syncOptions.force = true
+}
+
+db.sequelize.sync(syncOptions).then(function(){
+    app.listen(port, function() {
+    console.log(`Server now listening on PORT ${port}!`)
+  })
+})  
