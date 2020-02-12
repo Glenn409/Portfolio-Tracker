200fb3ec08844121993e7343dc493ff18b7dc73c
Christopher Glenn <Chrisglenn519@yahoo.com>
2020-02-12 17:38:14 -0600

    fixed server.js problems

---
+++ routes/account.js
@@ -0,0 +1,13 @@
+// const passport = require('passport')
+const db = require('../models')
+
+module.exports = app => {
+
+    app.get("/",(req,res) =>{
+        res.send('hellllooooo')
+    }) 
+
+
+
+    
+}
--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3001
 const Account = ('./routes/account')
 
 app.use(bodyParser.json())
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
 
-app.use('/account',Account)
+// app.use('/account',Account)
+require('./routes/account')(app)
 
 app.listen(port, () =>{
     console.log('app is listening on port: '+ port)
