033a41245ca4c915721680a0b3eb4f1eaf7d506c
Chris Glenn <chrisglenn519@yahoo.com>
2020-02-22 23:43:20 -0600

    updated db seeds

--- db/testSeeds/seeds.sql
+++ db/testSeeds/seeds.sql
@@ -4,6 +4,6 @@ use project3;
 -- INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','ltc', 96,'05-12-2019', null,NOW(),NOW(),2);
 
 INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','btc', 0.54, 1547272800000, null,NOW(),NOW(),2);
-INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','btc', 1.34, 1575504000, null,NOW(),NOW(),2);
-INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','btc', 0.14, 1610150400, null,NOW(),NOW(),2);
-INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','eth', 300.54, 1547272800000, null,NOW(),NOW(),2);
+INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','btc', 1.34, 1552608000, null,NOW(),NOW(),2);
+INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','btc', 0.14, 1579719931, null,NOW(),NOW(),2);
+INSERT INTO transactions (transaction_type,coin,quantity,purchaseDate,sellDate, createdAt,updatedAt,UserId) VALUES ('buy','eth', 300.54, 1579719931, null,NOW(),NOW(),2);
