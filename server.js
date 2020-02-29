608d55764494a96c46f128910dd32c044d8f8bd4
Christopher Glenn <Chrisglenn519@yahoo.com>
2020-02-29 05:51:59 -0600

    heroku

--- server.js
+++ server.js
@@ -13,11 +13,8 @@ const routeTools = require('./routes/routeFunctions')
 const path = require('path')
 
 if( process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
-
-    app.get('*',(req,res) =>{
-        res.sendFile(path.resolve(__dirname, 'client','index.html'))
-    })
+    app.use(express.static('build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve('build', 'index.html'));
 }
 app.use(bodyParser.json())
 app.use( bodyParser.urlencoded({extended: false}))
